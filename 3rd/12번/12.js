46eaa39f0118534104fbfb3e41785b357c277089
dodoman <rjsdn1955@naver.com>
2023-11-11 03:01:17 +0900

    3번째과제

--- 3rd/12번/12.js
+++ 3rd/12번/12.js
@@ -1,13 +1,13 @@
 const 투두 = {
   id: 1,
-  할일: "8시기상",
+  할일: '8시기상',
   완료여부: false,
 };
 
 // 여기에 코드를 작성해주세요.
-const newTodo = ?
+const newTodo = { ...투두, 완료여부: true };
 
-console.log(투두)
+console.log(투두);
 console.log(newTodo);
 // 결과
 // { id: 1, '할일': '8시기상', '완료여부': false }
