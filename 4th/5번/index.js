fd1ac3b2c49e4dddb31aca9fa99cdc42259fa775
dodoman <rjsdn1955@naver.com>
2023-11-14 15:55:51 +0900

    4회차과제저장

--- 4th/5번/index.js
+++ 4th/5번/index.js
@@ -1,3 +1,27 @@
 // 여기에 1번 문제의 답을 작성하세요.
-
+const getDatas = async () => {
+  try {
+    const resPonse = await fetch('https://dummyjson.com/products');
+    const data = await resPonse.json();
+    return data;
+  } catch (error) {
+    console.log('에러 발생', error);
+  }
+};
 // 여기에 2번 문제의 답을 작성하세요.
+const addData = async () => {
+  try {
+    const resPonse = await fetch('https://dummyjson.com/products/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title: 'BMW Pencil',
+        /* other product data */
+      }),
+    });
+    const daTa = await resPonse.json();
+    return daTa;
+  } catch (error) {
+    'error 발생했음', error;
+  }
+};
